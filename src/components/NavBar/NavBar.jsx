16a909ee324ad2f1d80d1bcb556ffbc13dd3e0f6
asp <amandasueputney@gmail.com>
2023-09-11 22:05:04 -0700

    updates nav bar and new logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,39 @@
+// import { Link } from 'react-router-dom';
+// import * as userService from '../../utilities/users-service';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+
+
+
+// export default function NavBar({ user, setUser }) {
+//   function handleLogOut() {
+//     userService.logOut();
+//     setUser(null);
+//   }
+
+//   return (
+//     <nav>
+//        <Navbar bg="dark" data-bs-theme="light">
+//         <Container>
+//           <img src="Logo.png" alt="logo" className="navLogo"/>
+//           <Navbar.Brand>Hi {user.name}</Navbar.Brand>
+//           <Nav className="me-auto">
+//           <Link to="/search/new">New Search</Link>
+//            &nbsp; | &nbsp;
+//           <Link to="/plan">Rotation Plans</Link>
+//           <Nav.Link to="" onClick={handleLogOut} class="align-rt" id='LogOut'>Log Out</Nav.Link>
+//         </Nav>
+//         </Container>
+//       </Navbar>
+//     </nav>
+//   );
+// }
+
+import React from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service'
+import * as userService from '../../utilities/users-service';
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
@@ -8,14 +42,22 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <span>Hi {user.name}</span>
-      &nbsp; | &nbsp;
-      <Link to="/search/new">New Search</Link>
-      &nbsp; | &nbsp;
-      <Link to="/plan">Rotation Plans</Link>
-      &nbsp; | &nbsp;
-      <Link to="" onClick={handleLogOut}>Log Out</Link>
-    </nav>
+    <Navbar bg="light" variant="light" fixed="top">
+      <Container>
+        <img src="Logo.png" alt="logo" className="navLogo"/>
+        <Navbar.Brand>Hi {user.name}</Navbar.Brand>
+        <Link to="/search/new" className="nav-link">
+          New Search
+        </Link>
+        <Link to="/plan" className="nav-link">
+          Rotation Plans
+        </Link>
+        <Nav className="ml-auto">
+          <Button variant="outline-light" onClick={handleLogOut}>
+            Log Out
+          </Button>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,13 +6,15 @@ export default function AuthPage({ setUser }) {
   const [showSignUp, setShowSignUp] = useState(false);
   return (
     <main>
-      <h1>AuthPage</h1>
-      <button onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
+      <img src="Logo.png" alt="logo" className="landinglogo"/>
+      <div></div>
+      {/* <h1>AuthPage</h1> */}
+      <button class="logIn" onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
       { showSignUp ?
           <SignUpForm setUser={setUser} />
           :
           <LoginForm setUser={setUser} />
-      }
+        }
     </main>
   );
 }
